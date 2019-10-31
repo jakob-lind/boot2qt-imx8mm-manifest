0a174e54b79d002efb8265c339e2f57bb74a166e
Samuli Piippo <samuli.piippo@qt.io>
2019-10-31 12:59:06 +0200

    Add manifests for previous releases
    Add manifests for all releases starting from Qt 5.9, including manifests
    for each active branch that always use HEAD of meta-boot2qt and meta-qt5
    layers.
    
    Change-Id: I22f0e237141b5173401f5e9257a8b87f03b6d1f3
    Reviewed-by: Kari Oikarinen <kari.oikarinen@qt.io>

---
+++ create-release-manifest.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# Copyright (C) 2019 The Qt Company Ltd.
+#
+# Turn manifest from meta-boot2qt into usable release manifest.
+set -e
+
+MANIFEST=$1
+
+if [ ! -f "${MANIFEST}" ]; then
+  echo "Manifest file not found: ${MANIFEST}"
+  exit 1
+fi
+
+REV=$(git rev-parse HEAD)
+
+# take out groups
+sed -i -e '/path/N;/external/s/\"\n.*/\"\/>/' ${MANIFEST}
+sed -i -e 's/,internal//' ${MANIFEST}
+
+# add meta-boot2qt
+if grep -q qtyocto ${MANIFEST}; then
+    sed -i -e '/<project name="meta-qt5"/i\
+\  <project name="meta-boot2qt"\
+           remote="qtyocto"\
+           revision="'${REV}'"\
+           path="sources/meta-boot2qt">\
+    <linkfile dest="setup-environment.sh" src="scripts/setup-environment.sh"/>\
+  </project>' ${MANIFEST}
+else
+    sed -i -e '/<project name="meta-qt5"/i\
+\  <project name="meta-boot2qt"\
+           remote="qt"\
+           revision="'${REV}'"\
+           path="sources/meta-boot2qt">\
+    <linkfile dest="setup-environment.sh" src="scripts/setup-environment.sh"/>\
+    <linkfile dest="sources/templates" src="meta-boot2qt-distro/conf"/>\
+  </project>' ${MANIFEST}
+fi
